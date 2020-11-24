d210faea83d9777b3cc31879263137dbcc60d1fa
Stephen Cochrane <skippycochrane@gmail.com>
2020-11-25 01:12:09 +0200

    Good progress

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
-import sys
-print(sys.__name__)
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route("/genkey", methods = ["GET"])
+def genkey():
+    nic = request.args.get('nic')
+    key = "TODO"
+
+    # TODO: Check that this user does not have a key
+    if True:
+        # TODO: Store this users key.
+        return render_template('key_success.html', nic=nic, key=key)
+    else:
+        error = str("Failed to generate a key for '%s', a key already exists." % nic)
+        return render_template('error.html', etype='key', error=error)
 
 def setup():
     app.template_folder = "../assets/templates/"
